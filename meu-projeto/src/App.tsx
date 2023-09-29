4a3892127d8c39fadce3121a54c51dae6c5fbb3e
danielcasanova12 <119464431+danielcasanova12@users.noreply.github.com>
2023-09-29 14:21:05 -0300

    Initial commit

---
+++ meu-projeto/src/App.tsx
@@ -0,0 +1,15 @@
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+
