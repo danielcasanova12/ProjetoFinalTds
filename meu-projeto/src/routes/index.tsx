061f79f0734f4e0c24b47b14ff7c78386d846862
danielcasanova12 <119464431+danielcasanova12@users.noreply.github.com>
2023-09-29 18:00:27 -0300

    feat: troca de tema

--- meu-projeto/src/routes/index.tsx
+++ meu-projeto/src/routes/index.tsx
@@ -1,13 +1,24 @@
 import {Routes , Route, Navigate} from 'react-router-dom'
 import {Button} from '@mui/material';
+import { useAppThemeContext } from '../shared/context';
 
-export const AppRoutes = () =>{
-    return(
+
+
+export const AppRoutes = () => {
+
+    const {toggleTheme} = useAppThemeContext();
+    return (
         <Routes>
-            <Route path='/pagina-inicial' element={<Button variant='contained' color='primary'>Button</Button>}/>
-
-
-            <Route path='*' element={<Navigate to='/pagina-inicial'></Navigate>}/>
+            {/* Route for the initial page */}
+            <Route 
+                path='/pagina-inicial' 
+                element={<Button variant='contained' color='primary' onClick={toggleTheme}>Button</Button>}
+            />
+            {/* Redirects to the initial page for all other paths */}
+            <Route 
+                path='*' 
+                element={<Navigate to='/pagina-inicial'></Navigate>}
+            />
         </Routes>
     );
 }
