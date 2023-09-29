3ec83e9b8a26c16b39303acddd8f88c5257624c0
danielcasanova12 <119464431+danielcasanova12@users.noreply.github.com>
2023-09-29 17:36:12 -0300

    Add material-ui

--- meu-projeto/src/routes/index.tsx
+++ meu-projeto/src/routes/index.tsx
@@ -1,10 +1,10 @@
 import {Routes , Route, Navigate} from 'react-router-dom'
-
+import {Button} from '@mui/material';
 
 export const AppRoutes = () =>{
     return(
         <Routes>
-            <Route path='/pagina-inicial' element={<p>Página inicial</p>}/>
+            <Route path='/pagina-inicial' element={<Button>Button</Button>}/>
 
 
             <Route path='*' element={<Navigate to='/pagina-inicial'></Navigate>}/>
